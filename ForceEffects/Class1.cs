f6ebd41e633e598fcb5093ac29874338c880b20e
braxtontalbert <braxton.talbert@gmail.com>
2023-02-04 17:34:03 -0500

    First test of gesture mod,, spawning item by pressing right hand grip button

--- ForceEffects/Class1.cs
+++ ForceEffects/Class1.cs
@@ -5,12 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using ThunderRoad;
-
+using SequenceTracker;
 
 namespace ForceEffects
 {
-    public class ForceEffectsEntry : SpellCastCharge
+    public class ForceEffectsEntry : SpellCastProjectile
     {
-        
+        Step root;
+        bool end;
+        public override void Load(SpellCaster spellCaster, Level level)
+        {
+            base.Load(spellCaster, level);
+            root = Step.Start(() => Player.currentCreature.handRight.HapticTick());
+
+            root.Then("Fist made", () => PlayerControl.handRight.gripPressed)
+                .Do(() => SpawnDagger());
+
+            end = root.AtEnd();
+            
+        }
+
+        void SpawnDagger() {
+
+            Catalog.GetData<ItemData>("DaggerCommon").SpawnAsync(item => {
+
+                item.transform.position = Player.currentCreature.handRight.transform.position;
+                
+            });
+        }
+
+        public override void UpdateCaster()
+        {
+            base.UpdateCaster();
+            if (end)
+            {
+                root.Reset();
+                return;
+            }
+            root.Update();
+            
+        }
+
     }
 }
